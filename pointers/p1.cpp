8a275d86ed41d6f71adfea83a3bbe07f3893e650
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-08-29 01:01:30 +0530

    new

--- pointers/p1.cpp
+++ pointers/p1.cpp
@@ -5,16 +5,36 @@ int main(){
 
     int num = 5;
 
+    // cout<< num << endl;
+    // // address of num 
+    // cout<< "address of num " << &num << endl;
+
+    // int *ptr = &num;
+
+    //  cout<< "value is at " << ptr << endl;
+
+    // cout<< "value is at " << *ptr << endl;
+
+    // double d = 2.4;
+
+    // double *p2 = &d;
+
+    // cout<< "size of pointer" << sizeof(double) <<  endl;
+    // cout<< "size of pointer" << sizeof(p2) <<  endl;
+    // cout<< "size of pointer" << sizeof(ptr) <<  endl;
+
+    int a  = num;
+    a++;
+
     cout<< num << endl;
-    // address of num 
-    cout<< "address of num " << &num << endl;
 
-    int *ptr = &num;
+    int *p = &num;
+    (*p)++;
+    cout<< num << endl;
+    // copying a pointer 
 
-     cout<< "value is at " << ptr << endl;
-
-    cout<< "value is at " << *ptr << endl;
-
+    int *q = p;
+    cout<< *q <<  " - " << *p << endl;
 
     return 0;
 }

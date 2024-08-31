8a8f32e7f53ca539577d5bc7c3ff4fd11f93dfb1
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-09-01 00:58:38 +0530

    first commit

--- reference dynamic/dynamic2d.cpp
+++ reference dynamic/dynamic2d.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
+    /*
     int n;
     cin>> n;
     int **arr = new int*[n];
@@ -11,7 +12,36 @@ int main(){
     // creatin done 
 
     for(int i=0; i<n; i++){
-        
+        for(int j = 0; j<n; j++){
+            cin>> arr[i][j];
+        }
+    }
+
+    //output
+    cout << endl;
+    for(int i=0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<  arr[i][j] << " ";
+        }cout << endl;
+    }
+    */
+
+   // when i != j ;
+
+   int row;
+   cin >> row;
+   int col;
+   cin>> col;
+
+    int ** arr = new int*[row];
+    for(int i=0; i<row; i++){
+        arr[i] = new int[col];
+    }
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> arr[i][j];
+        }
     }
     return 0 ;
 }

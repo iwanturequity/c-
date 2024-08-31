99ca15c436f125e56b9bb0638c44bf92003c9ae6
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-08-31 11:29:49 +0530

    first

--- .vscode/reference dynamic/reference.cpp
+++ .vscode/reference dynamic/reference.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
 using namespace std;
 
+// pass by reference 
+void update2(int &n){
+    n++;
+}
+void update1(int n){
+    n++;
+}
+
 int main(){
-
+    /*
     int i = 5;
-
     int &j = i;
-
     cout << i << endl;
-
     i++;
-
     cout<< i << endl;
-
     j++;
     cout<< j << endl;
+    */
 
+   int n = 5;
+    cout << " before " << n << endl;
+    update2(n);
+    cout << " after " << n <<endl;
+    
 
     return 0;
 }

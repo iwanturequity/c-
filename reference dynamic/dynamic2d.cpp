5d33ee73bc883d142f7bada0dc9313c0190a7d1c
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-09-01 00:42:54 +0530

    first commit

---
+++ reference dynamic/dynamic2d.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    int **arr = new int*[n];
+    for(int i=0; i<n; i++){
+        arr[i] = new int[n];
+    }
+
+    // creatin done 
+
+    for(int i=0; i<n; i++){
+        
+    }
+    return 0 ;
+}

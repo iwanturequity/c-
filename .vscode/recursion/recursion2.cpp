6304e3ff0fccd32d6341366fec0bafe8fe9d96c1
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-09-09 18:52:56 +0530

    new

--- .vscode/recursion/recursion2.cpp
+++ .vscode/recursion/recursion2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-
+/*
 void reachhome(int dest , int src){
 
     cout << "source " << src << " destination " <<  dest << endl;
@@ -20,6 +20,39 @@ int main(){
     int src = 0;
     
     reachhome( dest , src);
+    
+
+    return 0;
+}
+*/
+
+void sayDigit(int n , string arr[]){
+    // base case 
+
+    if( n == 0)
+        return ;
+    
+    //proccessing 
+
+    int digit = n%10 ;
+    n  = n/10;
+
+    //cout << arr[digit] << " " ;
+    //eske karn ulta print hoga  because yaha 2 1 4 aisa print hoga
+
+    sayDigit( n , arr);
+    cout << arr[digit] << " " ;
+}
+
+int main(){
+    int n ;
+    cin>>n;
+
+    string arr[10] = { "zero" , " one ", " two" , " three" ,
+     "four" , " five ", "six" , " seven " , " eight " , " nine " };
+
+    sayDigit( n , arr );
+
 
     return 0;
 }

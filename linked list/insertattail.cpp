6415f85edb5d409b0aaf259ef2b4917a180152f9
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-09-30 17:12:23 +0530

    added

--- linked list/insertattail.cpp
+++ linked list/insertattail.cpp
@@ -13,14 +13,61 @@ class node{
     }
 };
 
-void insertatPostition( int position , int data){
+// --------------- insertion at head -------------------;
+  void insertAtHead( node* &head , int data){ 
+    node* temp = new node(data);
+    temp -> next = head;
+    head = temp;
+  }
 
+// --------------- insertion at any position -------------------;
+void insertatPostition(node* &tail ,  node* &head , int position , int data){
+
+    if( position == 1){
+        insertAtHead(head , data);
+        return;
+    }
+    node* temp = head ;
+    int cnt = 1;
+    while( cnt < position-1){
+        temp = temp -> next;
+        cnt ++;
+    }
+
+    // if position is greater than length of linked list then insert at tail
+    if( temp -> next = NULL){
+        insertatTail(tail , data);
+        return;
+    }
+
+    // node creation 
+    node* nodeInsert = new node(data);
+    nodeInsert -> next = temp -> next;
+    temp -> next  = nodeInsert; 
 }
 
+// --------------- insertion at tail -------------------;
 void insertatTail( node* &tail , int data){
     node* temp = new node(data);
     tail -> next = temp;
     tail = tail -> next;
+}
+
+
+// --------------- delation from linklist -------------------;
+
+void deleteNode( int position , node* &head){
+    // deleting starting nide 
+    if( position == 1){
+        node* temp = head;
+        head = head -> next;
+    // memory free of stating node 
+        delete temp;
+    }
+    else{
+        // middle and last node delation 
+
+    }
 }
 
 void print( node* &head){
@@ -40,6 +87,12 @@ int main(){
     print(head);
 
     insertatTail(tail , 12);
+    print(head);
+
+    insertatTail(tail , 15);
+    print(head);
+
+    insertatPostition( tail ,head , 3 , 25);
     print(head);
 
     return 0;

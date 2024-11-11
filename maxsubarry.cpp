11e2297ed06447b490feed72e57b88d4b54ef279
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2024-11-12 01:48:48 +0530

    new

--- maxsubarry.cpp
+++ maxsubarry.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main(){
     int arr[5] = {1,2,3,4,5};
 
-    // for(int i = 0; i<5; i++){
-    //     for(int j=i+1; j<5; j++){
-    //         for(int k=i ; k<=j; k++){
-    //             cout << arr[k] << " " ;
-    //         }cout<<endl;
-    //     }
-    // }
+    for(int i = 0; i<5; i++){
+        for(int j=i+1; j<5; j++){
+            for(int k=i ; k<=j; k++){
+                cout << arr[k] << " " ;
+            }cout<<endl;
+        }
+    }
 
     // kendelen algorithm
     int carrySum = 0;

8aea1591446f131a7261e354c538cc5fbd99f6e9
Rajiv kumar <121884434+iwanturequity@users.noreply.github.com>
2025-01-08 12:46:28 +0530

    new

--- stack/stackarr.cpp
+++ stack/stackarr.cpp
@@ -12,12 +12,56 @@ class Stack{
     // Construct
     Stack(int size){
         this -> size = size;
+        arr = new int[size];
+        top = -1;
     }
 
-}
+    void push(int element){
+        if( size - top > 1){
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout << "Stack Overflow" << endl;
+        }
+    }
+    
+    void pop(){
+        if(top >= 0){
+            top--;
+        }else{
+            cout << "Stack Underflow" << endl;
+        }
+    }
+
+    int peek(){
+        if(top >= 0 ){
+            return(arr[top]);
+        }else{
+            cout << " stack is empty" << endl;
+            return -1;
+        }
+        
+    }
+
+    bool isEmpty(){
+        if( top == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+};
 int main(){
 
+    Stack s(5);
+    s.push(45);
+    s.push(17);
+    s.push(23);
+    s.pop();
 
+    cout << s.peek() << endl;
 
 
 

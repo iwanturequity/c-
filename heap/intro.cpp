626adbbb691121e87c1f99c08fb0be2472a49074
iwanturequity <your-email@example.com>
2025-03-23 01:13:42 +0530

    heap heapify

--- heap/intro.cpp
+++ heap/intro.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class heap{
@@ -52,7 +53,7 @@ class heap{
             int leftChild = 2*i;
             int rightChild = 2*i + 1;
 
-            if(leftChild<size && arr[i] < arr[i] <arr[leftChild]){
+            if(leftChild<size &&  arr[i] <arr[leftChild]){
                 swap(arr[i] , arr[leftChild]);
                 i = leftChild;
             }
@@ -66,6 +67,35 @@ class heap{
         }
     }
 };
+
+void heapify(int arr[] , int n, int i){
+    int largest = i;
+    int left = 2*i ;
+    int right = 2*i + 1;
+
+    if(left<=n && arr[largest] < arr[left] ){
+        largest = left;
+    }
+    if(right <=n && arr[largest] < arr[right]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[i], arr[largest]);
+        heapify(arr , n , largest);
+    }
+}
+
+// heap sort 
+void heapSort(int arr[], int n){
+    int size = n;
+
+    while(size>1){
+        swap(arr[size] , arr[1]);
+        size--;
+        heapify(arr , size , 1);
+    }
+}
 int main(){
 
     heap h;
@@ -75,6 +105,53 @@ int main(){
     h.insert(1);
     h.insert(9);
     h.print();
+
+    h.deletefromHeap();
+    h.print();
+
+    int arr[6] = {-1, 54, 55 , 53 , 52, 50};
+    int n =5;
+
+    // heap creation 
+    for(int i=n/2; i>0; i--) {
+        heapify(arr , n , i);
+    }
+    
+    cout<< " printing the array now " << endl;
+    for(int i=1; i<=n ; i++){
+        cout<< arr[i] << " ";
+    }cout<< endl;
+
+    // heapsort call
+    heapSort(arr, n);
+    cout<< " printing the sorted array " << endl;
+    for(int i=1; i<=n ; i++){
+        cout<< arr[i] << " ";
+    }cout<< endl;
+
+    // priority queue STK
+
+
+    // this give authomatic max heap
+    priority_queue<int>pq;
+    pq.push(1);
+    pq.push(4);
+    pq.push(7);
+    pq.push(2);
+
+    cout<< "the top element is " << pq.top() << endl;
+    pq.pop();
+    cout<< "the top element is " << pq.top() << endl;
+
+    // min heap
+    priority_queue<int, vector<int>, greater<int>>minHeap;
+    minHeap.push(1);
+    minHeap.push(4);
+    minHeap.push(7);
+    minHeap.push(2);
+    cout<< "the top element is " << minHeap.top() << endl;
+    minHeap.pop();
+    cout<< "the top element is " << minHeap.top() << endl;
 
     return 0;
 }
